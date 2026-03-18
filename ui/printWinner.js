b8713bfe3bd8f9f9f6e6d1090459d703b9fd672b
Andrei <andreykapas@gmail.com>
2026-03-18 17:14:15 +0100

    feat: add UI renderer for trick winner

---
+++ ui/printWinner.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * printWinner
+ * Created: 3/18/2026
+ */
+
+export function printWinner(trick) {
+  console.log(`Winner: Player ${trick.winner.playerId}`);
+  console.log('------------------');
+}
