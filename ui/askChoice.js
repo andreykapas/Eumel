6f334934f109c1e91f5ece3b972c0923e27cb2bb
Andrei <andreykapas@gmail.com>
2026-03-19 18:37:24 +0100

    refactor: remove UI logging from askChoice (prepare for UI separation)

--- ui/askChoice.js
+++ ui/askChoice.js
@@ -8,15 +8,7 @@ import { formatCard } from '../cards/index.js';
  * Created: 3/12/2026
  */
 
-export async function askChoice(message, options) {
-  console.log(`\n${message}\n`);
-
-  const cards = options
-    .map((card, i) => `${i + 1}) ${formatCard(card)}`)
-    .join('\n');
-
-  console.log(`Your hand:\n${cards}\n`);
-
+export async function askChoice({ message, options }) {
   while (true) {
     const answer = await ask('> ');
     const index = Number(answer) - 1;
@@ -24,7 +16,5 @@ export async function askChoice(message, options) {
     if (Number.isInteger(index) && index >= 0 && index < options.length) {
       return options[index];
     }
-
-    console.log('Invalid choice. Try again.');
   }
 }
